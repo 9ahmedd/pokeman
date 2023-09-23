bdbc1f6f73f267190a06bc071a737be30e5284bd
9ahmedd <ar20222020@gmail.com>
2023-09-23 17:49:54 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import PokemonList from './components/PokemonList';
+import Pagination from './components/Pagination';
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPageUrl, setCurrentPageUrl] = useState
+    ("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  useEffect(() => {
+    let cancel;
+    setLoading(true);
+    axios
+      .get(currentPageUrl, {
+        cancelToken : new axios.CancelToken((c)=>(cancel= c))
+      })
+      .then((response) => {
+        setPokemon(response.data.results.map((p) => p.name));
+        setLoading(false);
+        setNextPageUrl(response.data.next);
+        setPrevPageUrl(response.data.previous);
+      })
+      .catch((error) => {
+        console.log(error)
+        setLoading(false)
+      });
+    return () => {
+      cancel();
+    }
+  }, [currentPageUrl])
+  if (loading) return "...loading";
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage = {nextPageUrl ? goToNextPage : null}
+        goToPrevPage = {prevPageUrl ? goToPrevPage : null}
+      />
+    </>
+  )
 }
 
-export default App;
+export default App
